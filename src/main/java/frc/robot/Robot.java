b05b57bd056a5b28caf7cae7740cbb7ea977bca7
jakobschoeberle <jrschoeberle@gmail.com>
2025-10-04 22:08:56 -0500

    Added Elastic Dashboard and Basic Layout

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,11 @@
 
 package frc.robot;
 
+import edu.wpi.first.net.WebServer;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -15,6 +19,12 @@ public class Robot extends TimedRobot {
 
   public Robot() {
     m_robotContainer = new RobotContainer();
+  }
+
+  @Override
+  public void robotInit()
+  {
+    WebServer.start(5800, Filesystem.getDeployDirectory().getPath());
   }
 
   @Override
